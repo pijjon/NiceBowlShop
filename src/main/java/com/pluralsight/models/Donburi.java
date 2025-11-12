7bc0364ee895630c85f092bdc443a558459a30bc
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-12 16:09:25 -0500

    add topping filtering methods to Donburi

--- src/main/java/com/pluralsight/models/Donburi.java
+++ src/main/java/com/pluralsight/models/Donburi.java
@@ -1,5 +1,6 @@
 package com.pluralsight.models;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Donburi extends MenuItem {
@@ -11,6 +12,35 @@ public class Donburi extends MenuItem {
         this.size = size;
     }
 
-    
 
+
+    public List<Topping> getListOfPremium() {
+        return this.toppings.stream()
+                .filter(t -> t.type == ToppingType.PREMIUM)
+                .toList();
+    }
+
+    public List<Topping> getListOfAromaOil() {
+        return this.toppings.stream()
+                .filter(t -> t.type == ToppingType.AROMA_OIL)
+                .toList();
+    }
+
+    public List<Topping> getListOfRegular() {
+        return this.toppings.stream()
+                .filter(t -> t.type == ToppingType.REGULAR)
+                .toList();
+    }
+
+    public List<Topping> getListOfSauces() {
+        return this.toppings.stream()
+                .filter(t -> t.type == ToppingType.SAUCE)
+                .toList();
+    }
+
+    public List<Topping> getListOfSoup() {
+        return this.toppings.stream()
+                .filter(t -> t.type == ToppingType.SOUP)
+                .toList();
+    }
 }
