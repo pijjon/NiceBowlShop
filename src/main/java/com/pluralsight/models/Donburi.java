72b51a33a91011bc576ac41ca52e5a67c7bb03e2
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-12 09:03:06 -0500

    add size and toppings field to Donburi

--- src/main/java/com/pluralsight/models/Donburi.java
+++ src/main/java/com/pluralsight/models/Donburi.java
@@ -1,4 +1,9 @@
 package com.pluralsight.models;
 
+import java.util.List;
+
 public class Donburi extends MenuItem {
+    private int size; // 1,2,3 is S,M,L respectively
+    private List<Topping> toppings;
+
 }
