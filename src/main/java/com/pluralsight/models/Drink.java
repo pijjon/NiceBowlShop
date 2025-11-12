ff67c07f6113a3dbe95556c6a1948c020f523c70
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-12 15:05:16 -0500

    modify Drink class to use DrinkSize enum for cost calculating

--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -1,5 +1,15 @@
 package com.pluralsight.models;
 
 public class Drink extends MenuItem {
-    private String size;
+    private DrinkSize size;
+
+    public Drink(String name, DrinkSize size) {
+        super("Beer", size.getPrice()); // just beer for now until other drinks get added as an option
+        this.size = size;
+    }
+
+    @Override
+    public double getPrice() {
+        return this.basePrice;
+    }
 }
