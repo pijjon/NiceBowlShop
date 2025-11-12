b8848268e0f0c72d9cff67ae959d8c98b908d674
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-12 14:55:13 -0500

    create DrinkSize enum to keep track of sized-based pricing model

---
+++ src/main/java/com/pluralsight/models/DrinkSize.java
@@ -0,0 +1,20 @@
+package com.pluralsight.models;
+
+public enum DrinkSize {
+    // these statements implicitly call the constructor defined below to instantiate an enum with price property assigned
+    REGULAR(5.00),
+    LARGE(7.00);
+
+    // stores price for each enum instantiation
+    private final double price;
+
+    // implicitly private
+    DrinkSize(double price) {
+        this.price = price;
+    }
+
+    // method for retrieving the price based on size
+    public double getPrice() {
+        return price;
+    }
+}
