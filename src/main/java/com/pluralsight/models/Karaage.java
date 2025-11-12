6fa8e3335c9eb2e4ce30576317584bc32dd039b2
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-12 14:43:22 -0500

    create constructor and getPrice() method in Karaage class

--- src/main/java/com/pluralsight/models/Karaage.java
+++ src/main/java/com/pluralsight/models/Karaage.java
@@ -1,4 +1,13 @@
 package com.pluralsight.models;
 
 public class Karaage extends MenuItem {
+    public Karaage(String name, double price) {
+        super("Karaage Order", 6.00);
+
+    }
+
+    @Override
+    public double getPrice() {
+        return this.basePrice;
+    }
 }
