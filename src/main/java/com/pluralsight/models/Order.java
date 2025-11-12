f6e8b78f6101f6099fe78facfcd0a2f679caea91
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-12 02:04:07 -0500

    create Order class with placeholder

--- src/main/java/com/pluralsight/models/Order.java
+++ src/main/java/com/pluralsight/models/Order.java
@@ -1,4 +1,46 @@
 package com.pluralsight.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
+    protected String customer;
+    protected String orderName;
+    protected List<MenuItem> items = new ArrayList<>();
+
+    public Order(String customer) {
+        this.customer = customer;
+    }
+
+    public String getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(String customer) {
+        this.customer = customer;
+    }
+
+    public String getOrderName() {
+        return orderName;
+    }
+
+    public void setOrderName(String orderName) {
+        this.orderName = orderName;
+    }
+
+    public List<MenuItem> getItems() {
+        return items;
+    }
+
+    public void addItem(MenuItem item) {
+        this.items.add(item);
+    }
+
+    public double getOrderTotal() {
+        double total = 0;
+        // iterate through items and sum up each MenuItem price
+        // return sum
+        return total;
+    }
+
 }
