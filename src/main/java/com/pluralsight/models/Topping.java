0d64884b6ca159d9eb5135f41d2b8a2810a16424
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-13 17:39:38 -0500

    add constructor and getters to Topping class

--- src/main/java/com/pluralsight/models/Topping.java
+++ src/main/java/com/pluralsight/models/Topping.java
@@ -3,6 +3,19 @@ package com.pluralsight.models;
 import com.pluralsight.models.enums.ToppingType;
 
 public class Topping {
-    private String name;
-    protected ToppingType type;
+    private final String name;
+    protected final ToppingType type;
+
+    public Topping(String name, ToppingType type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public ToppingType getType() {
+        return type;
+    }
 }
