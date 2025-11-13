ad29d9da775fb64f5c14f08d683160cf17820813
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-13 03:18:15 -0500

    rename DonburiType enum names

--- src/main/java/com/pluralsight/models/enums/DonburiType.java
+++ src/main/java/com/pluralsight/models/enums/DonburiType.java
@@ -1,12 +1,12 @@
 package com.pluralsight.models.enums;
 
 public enum DonburiType {
-    GYU_DON("Savory-sweet beef & onions over rice."),
-    BUTA_DON("Tender pork in a sweet soy glaze on rice."),
-    OYAKO_DON("Chicken & egg simmered on rice."),
-    UNAGI_DON("Grilled eel with sweet sauce on rice."),
-    SAKE_DON("Fresh salmon sashimi over rice."),
-    YASAI_DON("Seasonal veggies lightly sautéed on rice.");
+    GYUDON("Savory-sweet beef & onions over rice."),
+    BUTADON("Tender pork in a sweet soy glaze on rice."),
+    OYAKODON("Chicken & egg simmered on rice."),
+    UNAGIDON("Grilled eel with sweet sauce on rice."),
+    SAKEDON("Fresh salmon sashimi over rice."),
+    YASAIDON("Seasonal veggies lightly sautéed on rice.");
 
     private String description;
 
