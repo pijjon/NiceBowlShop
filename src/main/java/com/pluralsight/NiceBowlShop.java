1169898f0ff77d3e0b4a7ceeafb22c7d0f301836
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-13 16:13:21 -0500

    start the program in main by calling ui.start()

--- src/main/java/com/pluralsight/NiceBowlShop.java
+++ src/main/java/com/pluralsight/NiceBowlShop.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
+import com.pluralsight.ui.UserInterface;
+
 public class NiceBowlShop {
+    public static void main(String[] args) {
+        UserInterface ui = new UserInterface();
+        ui.start();
+    }
 }
