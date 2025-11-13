a076887d299e8fdcfd9ee5a2c308270a133a919f
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-13 14:46:52 -0500

    update DonburiType enum to include a display name and getDisplayName() method

--- src/main/java/com/pluralsight/models/enums/DonburiType.java
+++ src/main/java/com/pluralsight/models/enums/DonburiType.java
@@ -1,21 +1,27 @@
 package com.pluralsight.models.enums;
 
 public enum DonburiType {
-    GYUDON("Savory-sweet beef & onions over rice."),
-    BUTADON("Tender pork in a sweet soy glaze on rice."),
-    OYAKODON("Chicken & egg simmered on rice."),
-    UNAGIDON("Grilled eel with sweet sauce on rice."),
-    SAKEDON("Fresh salmon sashimi over rice."),
-    YASAIDON("Seasonal veggies lightly sautéed on rice.");
+    GYUDON("Gyudon", "Savory-sweet beef & onions over rice."),
+    BUTADON("Butadon", "Tender pork in a sweet soy glaze on rice."),
+    OYAKODON("Oyakodon", "Chicken & egg simmered on rice."),
+    UNAGI_DON("Unagi Don", "Grilled eel with sweet sauce on rice."),
+    SAKE_DON("Sake Don", "Fresh salmon sashimi over rice."),
+    YASAI_DON("Yasai Don", "Seasonal veggies lightly sautéed on rice.");
 
-    private String description;
+    private final String description;
+    private final String displayName;
 
-    DonburiType(String description) {
+    DonburiType(String displayName, String description) {
         this.description = description;
+        this.displayName = displayName;
     }
 
     public String getDescription() {
         return description;
+    }
+
+    public String getDisplayName() {
+        return displayName;
     }
 
 }
