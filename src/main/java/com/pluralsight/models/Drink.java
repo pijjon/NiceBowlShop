463521250238b69a40fd9fa0a3f2ed5fa7fc158e
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-14 02:02:26 -0500

    modify Drink constructor to include DrinkName as a parameter

--- src/main/java/com/pluralsight/models/Drink.java
+++ src/main/java/com/pluralsight/models/Drink.java
@@ -1,12 +1,13 @@
 package com.pluralsight.models;
 
+import com.pluralsight.models.enums.DrinkName;
 import com.pluralsight.models.enums.DrinkSize;
 
 public class Drink extends MenuItem {
     private DrinkSize size;
 
-    public Drink(String name, DrinkSize size) {
-        super("Beer", size.getPrice()); // just beer for now until other drinks get added as an option
+    public Drink(DrinkName drinkName, DrinkSize size) {
+        super(drinkName.name(), size.getPrice()); // just beer for now until other drinks get added as an option
         this.size = size;
     }
 
