df07a6c55e6775809e0b27be97d16a372a8404c9
Alwin Zhao <alwinzhao12@gmail.com>
2025-11-14 02:04:30 -0500

    update Topping constructor

--- src/main/java/com/pluralsight/models/Topping.java
+++ src/main/java/com/pluralsight/models/Topping.java
@@ -1,13 +1,14 @@
 package com.pluralsight.models;
 
+import com.pluralsight.models.enums.ToppingItem;
 import com.pluralsight.models.enums.ToppingType;
 
 public class Topping {
     private final String name;
     protected final ToppingType type;
 
-    public Topping(String name, ToppingType type) {
-        this.name = name;
+    public Topping(ToppingItem toppingItem, ToppingType type) {
+        this.name = toppingItem.getDisplayName();
         this.type = type;
     }
 
